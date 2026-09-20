a94593a8cdbeb432d7803de0aa2cde0aa9b16882
slammers001 <slammers@einhorn.co>
2026-09-20 18:40:44 -0400

    add posthog

---
+++ public/config.js
@@ -0,0 +1,3 @@
+// PostHog Configuration
+const POSTHOG_API_KEY = 'phc_dOBViKPhL2wwSDvkWprVr9vmD5L5303U10sVxcqda3T'; //yes, this is purposely public
+const POSTHOG_HOST = 'https://us.i.posthog.com';
